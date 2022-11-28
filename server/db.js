06c93dc4cc4bafc880dc09862b72f9d5be0d0eca
AslimTaslam <50074573+AslimTaslam@users.noreply.github.com>
2022-11-28 07:18:18 +0300

    Edit pass

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const {Pool} = require("pg");
 
 const pool = new Pool({
 	user: "postgres",
-	password: "zara51",
+	password: "yourpassword",
 	host: "localhost",
 	port: 5432,
 	database: "short_link",
